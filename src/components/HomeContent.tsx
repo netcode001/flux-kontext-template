6b1c431525f20b6a06ef2ee36c32bcb4c4293dd4
Alex Li <lylh0319@126.com>
2025-07-01 17:08:49 +0800

    删除主页 banner（HeroCarousel 轮播图区域），仅保留新闻、视频和功能区，代码已中文注释。

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -5,6 +5,7 @@ import { Footer } from "@/components/Footer"
 import { HeroCarousel } from "./home/HeroCarousel"
 import { NewsSection } from "./home/NewsSection"
 import { FeatureCards } from "./home/FeatureCards"
+import { VideoCard } from "./home/VideoCard"
 
 export function HomeContent() {
   return (
@@ -14,15 +15,18 @@ export function HomeContent() {
 
       {/* 主要内容区域 */}
       <main className="pt-16">
-        {/* 轮播图区域 */}
-        <section className="py-8">
+        {/* 轮播图区域已删除，原为 HeroCarousel 主页 banner */}
+        {/* <section className="py-8">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <HeroCarousel />
           </div>
-        </section>
+        </section> */}
 
         {/* 新闻区域 */}
         <NewsSection />
+
+        {/* 视频区 */}
+        <VideoCard />
 
         {/* 功能卡片区域 */}
         <FeatureCards />
